9f040bc955d00a786e2dd5fdaac50b51908c8627
Alen Cyriac Joseph <semicologne13@outlook.com>
2025-01-14 19:48:56 +0530

    feat(auth): update login form
    - Enhanced the login form for better user experience and functionality.

--- src/components/DialogLoginForm.tsx
+++ src/components/DialogLoginForm.tsx
@@ -8,8 +8,6 @@ import {
   DialogTrigger
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 
 import GithubSignInButton from "./GithubSignInButton";
 
@@ -24,38 +22,24 @@ export default function DialogLoginForm() {
           Sign In
         </Button>
       </DialogTrigger>
-      <DialogContent className="flex flex-col min-h-[420px] max-w-sm items-center justify-center p-2 md:p-4">
+      <DialogContent className="flex flex-col min-h-[420px] max-w-sm items-center justify-center p-8 md:p-4">
         <DialogHeader>
           <DialogTitle className="text-2xl font-bold">Login to your account</DialogTitle>
         </DialogHeader>
-        <form className="gap-6">
+        <div className="gap-6">
           <div className="flex flex-col items-center gap-1 text-center">
             <p className="text-md font-bold">Project Management Made Simple</p>
             <p className="text-sm text-muted-foreground">
               Streamline your workflow with Project Pilot
             </p>
           </div>
-          <div className="grid gap-6 w-full mt-3">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com"/>
-            </div>
-            <Button 
-              type="submit"
-              className="w-full"
-              variant={'login'}
-              size={'login'}
-            >
-              Login
-            </Button>
-            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-              <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
+          <div className="p-6 relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+              Continue with
+            </span>
           </div>
-        </form>
-        <GithubSignInButton/>
+          <GithubSignInButton/>
+        </div>
       </DialogContent>
     </Dialog>
   );
